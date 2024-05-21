46318b9ececd87f38748af233c7e22327fd57d34
Kyle <khang194591@gmail.com>
2024-05-21 18:00:17 +0700

    update

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -12,13 +12,17 @@ import { PrismaService } from 'src/config';
 import { CHAT_PATTERNS, IMemberResDto } from 'src/libs';
 import { CurrentMember } from 'src/shared';
 import { AuthGuard } from '../auth/auth.guard';
+import { NotificationService } from '../services/notification.service';
 import { SendMessageDto } from './chat.dto';
 
 @WebSocketGateway(3001, {
-  cors: { origin: 'http://localhost:5173', credentials: true },
+  cors: { origin: process.env.CORS_WHITELIST.split(','), credentials: true },
 })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly notificationService: NotificationService,
+  ) {}
 
   handleConnection(client: Socket) {
     console.log(`Client ${client.id} connected`);
@@ -41,7 +45,9 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     const { content, channelId } = data;
     const senderId = member.id;
 
-    const message = await this.prismaService.message.create({
+    console.log(`${senderId} sent to ${channelId}: ${content}`);
+
+    const message = await this.prisma.message.create({
       data: {
         content,
         senderId,
@@ -54,5 +60,40 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
       CHAT_PATTERNS.RECEIVE_MESSAGE + channelId,
       message,
     );
+
+    const offlineMembers = await this.prisma.channelMember.findMany({
+      where: { channelId, online: false },
+    });
+
+    if (offlineMembers.length) {
+      try {
+        console.log(offlineMembers);
+
+        await Promise.all(
+          offlineMembers
+            .filter(({ memberId }) => memberId !== senderId)
+            .map((member) =>
+              this.notificationService.sendWebPushNotification({
+                memberId: member.memberId,
+                title: 'You have new message',
+                content: message.content,
+                link: `/chat/${member.channelId}`,
+              }),
+            ),
+        );
+      } catch (error) {
+        console.log('Error when sending notification');
+        console.log(error);
+      }
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @SubscribeMessage(CHAT_PATTERNS.PING)
+  async ping(@CurrentMember() member: IMemberResDto) {
+    await this.prisma.channelMember.updateMany({
+      where: { memberId: member.id },
+      data: { online: true },
+    });
   }
 }
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,5 @@
 import { ChatModule } from './chat/chat.module';
+import { CronModule } from './cron/cron.module';
 import { microServiceModules } from './services';
 
-export const modules = [ChatModule, ...microServiceModules];
+export const modules = [CronModule, ChatModule, ...microServiceModules];
